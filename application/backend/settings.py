d27b754851e6deebad57260f8c8aef678a803c2c
ajarm <a.jarmillan@gmail.com>
2026-03-27 18:30:55 -0400

    more configs

--- application/backend/settings.py
+++ application/backend/settings.py
@@ -111,6 +111,11 @@ DATABASES = {
     )
 }
 
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SAMESITE = 'None'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
