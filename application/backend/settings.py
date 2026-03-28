db4ba8aca600d5be6688a8ef2369d77a4e2d7cb5
Abbey McMillan <a.jarmillan@gmail.com>
2026-03-28 19:29:46 -0400

    fixing cors configs

--- application/backend/settings.py
+++ application/backend/settings.py
@@ -16,6 +16,7 @@ from dotenv import load_dotenv
 import os
 import dj_database_url
 load_dotenv()
+import re
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,17 +87,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    # "*"
-]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = os.environ.get(
+    "CORS_ALLOWED_ORIGINS", 
+    "http://localhost:5173,http://127.0.0.1:5173"
+).split(",")
+
+
+CORS_ALLOW_ALL_ORIGINS = os.environ.get("CORS_ALLOW_ALL_ORIGINS", "False") == "True"
+
 
 CSRF_TRUSTED_ORIGINS = os.environ.get(
     "CSRF_TRUSTED_ORIGINS", 
     "http://localhost:3000,http://localhost:8000,http://127.0.0.1:8000"
 ).split(",")
+
+CORS_ALLOWED_ORIGIN_REGEXES = []
+cors_regex = os.environ.get("CORS_ALLOWED_REGEX")
+if cors_regex:
+    CORS_ALLOWED_ORIGIN_REGEXES.append(re.compile(cors_regex))
 
 # CORS_ALLOWED_ORIGINS=["*"] # for production
 
