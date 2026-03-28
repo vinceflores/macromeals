718860f6cfbfa30a4c769e293b14365aceb97728
Abbey McMillan <a.jarmillan@gmail.com>
2026-03-27 22:39:35 -0400

    removing avatar error

--- presentation/components/user-navigation.tsx
+++ presentation/components/user-navigation.tsx
@@ -25,7 +25,6 @@ export function UserNav({ email, first_name, last_name }: UserNavProperties) {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" />
             <AvatarFallback>{initials}</AvatarFallback>
           </Avatar>
         </Button>
